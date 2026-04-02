ae161fb84f8b54612969a2965cc6ae633707ee59
BigKille <djittedjibril212@gmail.com>
2026-04-03 03:29:35 +0530

    Add files via upload

---
+++ kmeans_iris_short.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import datasets
+
+# Load the Iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Perform k-means clustering
+k = 3  # number of clusters
+kmeans = KMeans(n_clusters=k, random_state=0).fit(X)
+
+# Get cluster labels
+labels = kmeans.labels_
+
+
+# Visualize the clusters (using first two features)
+plt.scatter(X[:, 0], X[:, 1], c=labels, s=50, cmap='viridis')
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75)
+plt.title('KMeans Clustering of Iris Dataset')
+plt.xlabel('Sepal Length (cm)')
+plt.ylabel('Sepal Width (cm)')
+plt.show()
